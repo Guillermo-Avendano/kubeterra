7a7c0b1b33dab596626dd26dc1471f3feabd548f
Guillermo Avendano <gavendano@rocketsoftware.com>
2025-12-03 16:10:41 -0300

    va

--- 05_terraform.sh
+++ 05_terraform.sh
@@ -28,6 +28,9 @@ helm repo update;
 
 cd terra/kube
 log INFO "Initializing Terraform..."
+
+chmod -R +x .terraform/providers/
+
 terraform init
 
 log INFO "running : 'terraform apply -auto-approve'"
@@ -49,4 +52,4 @@ terraform apply \
 -var=var_docker_email='gavendano@rs.com' \
 -var=var_mobius_license='01MOBIUS52464A464C4BF55859518381908FAEA4434F46515E53539681955B454D6240534556564351471D454D12405303565672514759454D1640530556560B51470E454D6040537C56560D514715454D1040536556560351470A454D0540531356560951472A454D2A40531556561D5442BBB6BC5940531A5C53A6A2B6BAB6BC5D40531A5C53A6A2B6BAB6BC2840533456561D5B42BBB6BCBBB3A23556561D5B42BBB6BCBBB3A23656563E514720454D2040535B5055F4AA8D' \
 -var=var_smart_chat_openai_api_key=$OPENAI_KEY \
--var=var_pvc_storage_class='nfs-csi' | tee terraform.log
+-var=var_pvc_storage_class='nfs-storage' | tee terraform.log
